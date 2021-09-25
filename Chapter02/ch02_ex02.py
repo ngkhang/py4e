0ad1abf584d1667a9c26f249f6b3454cb39bf8a6
ngkhang <khangnguyen26022000@gmail.com>
2021-09-25 15:37:28 +0700

    Update file cha02_ex02.py

--- Chapter02/ch02_ex02.py
+++ Chapter02/ch02_ex02.py
@@ -1,3 +1,10 @@
+'''
+Exercise 2:
+    Write a program that uses input to\
+        prompt a user for their name and
+        then welcomes them.
+'''
+
 # the code below almost works
 name = input('Enter your name: ')
 print('Hello',name)
